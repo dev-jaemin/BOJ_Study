fa6f972cfa7fd039b4dfbb6d4c84c0fe2533a9c6
Jaemin Kim <jay.kimjaemin@gmail.com>
2021-03-11 10:10:39 +0000

    1260

---
+++ src/210308_210314/1260.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+
+
+using namespace std;
+
+int n, m, v, a, b;
+vector<int> adj[1010];
+bool visited[1010];
+
+void makeAdjList(int count, int size){
+    for(int i=1;i<=count;i++){
+        cin >> a >> b;
+        
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    
+    for(int i=1;i<=size;i++){
+        if(!adj[i].empty()) sort(adj[i].begin(), adj[i].end());
+    }
+}
+
+void dfs(int start){
+    stack<int> s;
+    
+    visited[start] = true;
+    
+    cout << start << ' ';
+    
+    for(int i=0;i<adj[start].size();i++){
+        int nextNode = adj[start][i];
+        if(!visited[nextNode]){
+            dfs(nextNode);
+        }
+    }
+    
+    
+}
+
+void bfs(int start){
+    queue<int> q;
+    
+    visited[start] = true;
+    
+    q.push(start);
+    
+    while(!q.empty()){
+        int curNode = q.front();
+        q.pop();
+        cout << curNode << ' ';
+        
+        for(int i=0;i<adj[curNode].size();i++){
+            int nextNode = adj[curNode][i];
+            
+            if(!visited[nextNode]){
+                q.push(nextNode);
+                visited[nextNode] = true;
+            }
+        }
+    }
+    
+}
+
+
+int main(int argc, char* argv[]) {
+	ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n >> m >> v;
+    
+    makeAdjList(m, n);
+    
+    dfs(v);
+    cout << '\n';
+    
+    for(int i=1;i<=n;i++){
+        visited[i] = false;
+    }
+    
+    bfs(v);
+    cout << '\n';
+
+    
+	
+	return 0;
+}
