a1868fab2e9a7d13bf0c1fb59f1bbac2b71b840e
Jaemin Kim <jay.kimjaemin@gmail.com>
2021-06-24 14:14:46 +0000

    solving insertion

--- src/algospot/insertion.cpp
+++ src/algospot/insertion.cpp
@@ -3,15 +3,12 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <cassert>
 
 
 using namespace std;
 
 typedef int KeyType;
-typedef pair<Node*, Node*> NodePair;
-
-
-int n;
 
 struct Node{
     KeyType key;
@@ -30,7 +27,9 @@ struct Node{
         if(left) size += left->size;
         if(right) size += right->size;
     }
-}
+};
+
+typedef pair<Node*, Node*> NodePair;
 
 NodePair split(Node* root, KeyType key){
     if(root == NULL){
@@ -61,17 +60,17 @@ Node* insert(Node* root, Node* newNode){
     if(root->priority < newNode->priority){
         NodePair sp = split(root, newNode->key);
         
-        newNode.setLeft(sp.first);
-        newNode.setRight(sp.second);
+        newNode->setLeft(sp.first);
+        newNode->setRight(sp.second);
         
         return newNode;
     }
     else{
-        if(root->key < newNode->key){
-            root->setLeft(insert(root->left, newNode));
+        if(root->key <= newNode->key){
+            root->setRight(insert(root->right, newNode));
         }
         else{
-            root->setRight(insert(root->right, newNode));
+            root->setLeft(insert(root->left, newNode));
         }
     }
     
@@ -79,30 +78,87 @@ Node* insert(Node* root, Node* newNode){
 }
 
 
-Node* merge(Node* a, Node* b);
+Node* merge(Node* a, Node* b){
+    if(a == NULL) return b;
+    if(b == NULL) return a;
+    
+    if(a->priority < b->priority){
+        b->setLeft(merge(a, b->left));
+        return b;
+    }
+    a->setRight(merge(a->right, b));
+    return a;
+}
 
 
 Node* erase(Node* root, KeyType key){
     if(root == NULL) return root;
     
     if(root->key < key){
-        root->left(erase(root->left, key));
+        root->setLeft(erase(root->left, key));
     }
     else if(root->key > key){
-        root->right(erase(root->right, key));
+        root->setRight(erase(root->right, key));
     }
     else{
         Node* ret = merge(root->left, root->right);
         delete root;
         return ret;
     }
+    
+    return root;
 }
+
+Node* kth(Node* root, int k){
+    int leftSize = 0;
+    
+    if(root->left != NULL) leftSize = root->left->size;
+    
+    if(k<=leftSize) return kth(root->left, k);
+    if(leftSize == k - 1){
+        return root;
+    }
+    else{
+        return kth(root->right, k - leftSize - 1);
+    }
+    
+}
+
 
 
 int main(int argc, char* argv[]) {
 	ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    
+    int c;
+    cin >> c;
+
+    while(c--){
+        int n = 0;
+        cin >> n;
+        
+        vector<int> shifted(n);
+        vector<int> ans(n);
+        Node* root = NULL;
+        
+        for(int i=0;i<n;i++){
+            cin >> shifted[i];
+            
+            root = insert(root, new Node(i+1));
+        }
+        
+        for(int i = n-1; i>=0; i--){
+            int larger = shifted[i];
+            Node* k = kth(root, i+1-larger);
+            ans[i] = k->key;
+            root = erase(root, k->key);
+        }
+        for(int i=0;i<n;i++) cout << ans[i] << ' ';
+        cout << '\n';
+        
+    }
+    
 	
 	return 0;
 }
